ea433c381303077e44e57bde915cf8b06a11b1ec
Mehar Fatma <mehar.fatma202@gmail.com>
2024-05-24 01:13:33 +0530

    base setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,12 +5,19 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "OTAKU",
-  description: "otaku",
+  description: "OTAKU",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Julee&family=Roboto:wght@900&display=swap" rel="stylesheet"></link>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-// import Image from "next/image";
+import React from 'react';
 
-export default function App() {
+const Home = () => {
   return (
     <main className="text-center text-xl font-semibold">
       <h1>HELLO THERE!</h1>
     </main>
   );
 }
+
+export default React.memo(Home);
